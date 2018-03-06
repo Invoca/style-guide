88d297efdb3d365002fb5a64b800fea4a157099e
Alec Jacobs <ajacobs@invoca.com>
2018-03-05 16:57:40 -0800

    pull in jshint

--- javascript/example.js
+++ javascript/example.js
@@ -8,4 +8,16 @@ class HiHound {
   get comments() {
     let comment = this.map(elements, (el) => $(el).text());
   }
+
+  test() {
+    var hey = "hi"
+
+    return hey
+  }
+
+  test2() {
+    let hey = "hi";
+
+    return hey;
+  }
 }
